ece5a8e3ed072df7c47943412cb489d547eabfaf
woods <woodsonspring@gmail.com>
2025-04-24 17:18:56 -0400

    updated oddOrEven

--- src/woodson/OddOrEven.java
+++ src/woodson/OddOrEven.java
@@ -2,8 +2,16 @@ package woodson;
 
 public class OddOrEven {
     public static void main(String[] args) {
+        oddOrEven(5);
 
-        int number = 5;
+
+    }
+
+
+    public static void oddOrEven(int number ) {
+
+
+
         if (number % 2 == 0) {
             System.out.println(number + " is Even");
         } else {
