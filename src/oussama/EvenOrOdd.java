44865b06e88286c2018b227e897fea0d96c8979a
elbakkali2 <oussama.elbakkali@icloud.com>
2025-04-24 16:25:25 -0400

    new one

--- src/oussama/EvenOrOdd.java
+++ src/oussama/EvenOrOdd.java
@@ -2,5 +2,18 @@ package oussama;
 
 public class EvenOrOdd {
 
-    
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void evenOrOdd(int num) {
+        if (num % 2 == 0) {
+            System.out.println("even");
+        }else if (num % 2 == 1){
+            System.out.println("odd");
+        }else{
+            System.out.println("invalid");
+        }
+    }
 }
