37a943a1ac9f0c00d8d251345a4cac487e6c18c3
anthonycelis909 <anthonycelis909@gmail.com>
2025-04-24 14:34:39 -0700

    OddOrEven class complete

--- src/Anthony/Week1_OddOrEven.java
+++ src/Anthony/Week1_OddOrEven.java
@@ -4,11 +4,19 @@ public class Week1_OddOrEven {
 
     public static void main(String[] args) {
 
-        oddOrEven();
+        oddOrEven(2);
 
     }
 
-    public static void oddOrEven(){
+    public static void oddOrEven(int num){
+
+        if (num % 2 == 0){
+            System.out.println("even");
+        } else if (num % 2 == 1) {
+            System.out.println("odd");
+        }else{
+            System.out.println("zero");
+        }
 
     }
 }
