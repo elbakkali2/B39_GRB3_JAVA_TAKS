a7ee5f545a60db3d61382a7d569fde15166063ae
Nigar <ge6946639@gmail.com>
2025-04-26 12:18:05 -0400

    Nigar creat Package week1 class

--- src/Nigar/week1OddOrEvent.java
+++ src/Nigar/week1OddOrEvent.java
@@ -1,4 +1,20 @@
 package Nigar;
 
 public class week1OddOrEvent {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void OddOrEven(int number){
+
+        if (number%2==0){
+            System.out.println(number + " is Even");
+        }else if (number%2==1){
+            System.out.println(number + " is Odd");
+        }else{
+            System.out.println(number + " is 0");
+        }
+
+    }
 }
