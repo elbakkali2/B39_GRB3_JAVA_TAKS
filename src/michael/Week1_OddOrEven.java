ef9278c3699459c3131db68c6d26361a330ed12d
Ersin <michael.knight@cydeo.com>
2025-04-26 19:30:26 +0300

    OddOrEven Class is updated_2.

--- src/michael/Week1_OddOrEven.java
+++ src/michael/Week1_OddOrEven.java
@@ -2,11 +2,11 @@ package michael;
 
 public class Week1_OddOrEven {
     public static void main(String[] args) {
-        oddOrEven();
+        oddOrEven(5);
 
     }
 
-    public static void oddOrEven(){
+    public static void oddOrEven(int num){
 
     }
 }
