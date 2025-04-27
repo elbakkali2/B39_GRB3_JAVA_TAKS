f2287bcb20dea2bdc20520747ee69b139c559c7c
Ersin <michael.knight@cydeo.com>
2025-04-27 20:39:18 +0300

    Added my solution for OddOrEven question.

--- src/michael/Week1_OddOrEven.java
+++ src/michael/Week1_OddOrEven.java
@@ -3,10 +3,18 @@ package michael;
 public class Week1_OddOrEven {
     public static void main(String[] args) {
         oddOrEven(5);
+        oddOrEven(52);
 
     }
 
     public static void oddOrEven(int num){
 
+        if (num%2==0){
+            System.out.println(num + " is Even.");
+        } else if (num%2==1){
+            System.out.println(num + " is Odd.");
+        }else {
+            System.out.println(num + " is Zero.");
+        }
     }
 }
