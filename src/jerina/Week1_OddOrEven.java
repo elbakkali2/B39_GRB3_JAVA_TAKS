386b0e509cf2adeb9e8e49cb61dceb0c09c91c7e
dardhajerina <dardha.jerina@gmail.com>
2025-05-01 16:57:46 -0400

    Added another number to test

--- src/jerina/Week1_OddOrEven.java
+++ src/jerina/Week1_OddOrEven.java
@@ -3,16 +3,18 @@ package jerina;
 public class Week1_OddOrEven {
     public static void main(String[] args) {
 OddOrEven(4);
+OddOrEven(2);
+OddOrEven(11);
 
     }
     public static void OddOrEven(int num) {
 
-        if(num % 2 == 0) {
-            System.out.println("The number "+ num +" is odd");
-        } else if  (num % 2 == 1) {
+        if(num == 0) {
+            System.out.println("The number "+ num + "is zero");
+        } else if  (num % 2 == 0) {
             System.out.println("The number " + num + " is even");
         } else {
-            System.out.println("The number " + num + " is zero");
+            System.out.println("The number " + num + " is odd");
         }
 
     }
