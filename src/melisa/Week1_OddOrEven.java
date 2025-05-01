3d994457d3d6a5e91947a1d248832289abbe03b0
melisa <karakoyuncumelisa@gmail.com>
2025-05-01 15:16:23 -0500

    tested oddOrEven method by calling the method inside the main method.

--- src/melisa/Week1_OddOrEven.java
+++ src/melisa/Week1_OddOrEven.java
@@ -4,16 +4,31 @@ public class Week1_OddOrEven {
 
     public static void main(String[] args) {
 
-        oddOrEven();
+        oddOrEven(7);
 
-        oddOrEven();
+        oddOrEven(8);
 
+        oddOrEven(0);
+
+        oddOrEven(-5);
+
+        oddOrEven(-6);
+
+        //oddOrEven(5.5);
     }
 
-    public static void oddOrEven(){
+    public static void oddOrEven(int number) {
 
+        if (number == 0) {
+            System.out.println(number + " is an even number, but zero is a special number.");
+        } else if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
 
     }
-
-
 }
+
+
+
