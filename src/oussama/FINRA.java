e6b8d39bd492f931607dd0ff7383a94b37b7fb02
elbakkali2 <oussama.elbakkali@icloud.com>
2025-05-08 15:56:19 -0400

    Finra task is updated

--- src/oussama/FINRA.java
+++ src/oussama/FINRA.java
@@ -3,8 +3,9 @@ package oussama;
 public class FINRA {
     public static void main(String[] args) {
 
-        int num = 30;
-
+        fINRA(1000);
+    }
+    public static void fINRA(int num){
         for (int i = 1; i <= num; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.print("FINRA");
@@ -17,18 +18,19 @@ public class FINRA {
             } else {
                 System.out.print(" " + i + " ");
             }
-        }
 
         }
-
-        //Write a method which prints out the numbers
-        // from 1 to 30 but if the number is a multiple of 3,
-        // print "FIN" instead of the number and if the number is multiple of 5,
-        // print "RA" instead of the number. If the number is a multiple of both 3
-        // and 5, then print "FINRA" instead of the number.
-        //Example:
-        //1 2 FIN 4 RA FIN 7 8 FIN RA 11 FIN 13 14 FINRA ...
-
 
     }
+
+    //Write a method which prints out the numbers
+    // from 1 to 30 but if the number is a multiple of 3,
+    // print "FIN" instead of the number and if the number is multiple of 5,
+    // print "RA" instead of the number. If the number is a multiple of both 3
+    // and 5, then print "FINRA" instead of the number.
+    //Example:
+    //1 2 FIN 4 RA FIN 7 8 FIN RA 11 FIN 13 14 FINRA ...
+
+
+}
 
