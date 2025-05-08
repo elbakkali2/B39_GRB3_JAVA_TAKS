0ff0e1299fdc7f1eb62032df4d46cf82ff8cf126
melisa <karakoyuncumelisa@gmail.com>
2025-05-08 14:32:59 -0500

    Added FINRA class, created method and used it in the main method.

---
+++ src/melisa/FINRA.java
@@ -0,0 +1,24 @@
+package melisa;
+
+public class FINRA {
+
+    public static void main(String[] args) {
+
+        finra();
+
+    }
+
+    public static void finra() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FINRA ");
+            } else if (i % 3 == 0) {
+                System.out.print("FIN ");
+            } else if (i % 5 == 0) {
+                System.out.print("RA ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
