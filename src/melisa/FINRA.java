8d2523e4ca75487271a94e9c9d1bbf822db5fc09
melisa <karakoyuncumelisa@gmail.com>
2025-05-10 11:30:04 -0500

    Added overloading method for finra.

--- src/melisa/FINRA.java
+++ src/melisa/FINRA.java
@@ -5,6 +5,8 @@ public class FINRA {
     public static void main(String[] args) {
 
         finra();
+        System.out.println();
+        finra(15);
 
     }
 
@@ -20,5 +22,21 @@ public class FINRA {
                 System.out.print(i + " ");
             }
         }
+    }
+
+
+    public static void finra(int i) {
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            System.out.println("FINRA");
+        } else if (i % 5 == 0) {
+            System.out.println("RA");
+        } else if (i % 3 == 0) {
+            System.out.println("FIN");
+        }else{
+            System.out.print(i + " ");
+        }
+
+
     }
 }
