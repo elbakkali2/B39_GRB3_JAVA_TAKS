56c19ba716b4169979686a0f98e4c1d386df7d7c
aisha <aishaemin33@gmail.com>
2025-05-11 19:01:44 -0400

    OddOrEven is created!

--- src/maryna/OddOrEven.java
+++ src/maryna/OddOrEven.java
@@ -1,25 +1,25 @@
-package maryna;
+package Maryna;
 
 public class OddOrEven {
 
     public static void main(String[] args) {
-
-        oddOrEven(1);
-        oddOrEven(10);
-        oddOrEven(-10);
-        oddOrEven(0);
+        evenOrOdd(1);
+        evenOrOdd(2);
+        evenOrOdd(0);
 
     }
 
+    public static void evenOrOdd(int num) {
+        if(num==0) {
+            System.out.println(num + " is an even number, but zero is a special number.");
+        }else if (num % 2 == 0) {
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
 
-    public static void oddOrEven (int num) {
-        if ( num == 0 ) {
-            System.out.println(num + " is an even number, bu zero is a special character.");
-        } else if ( num % 2 == 0 ) {
-            System.out.println(num + " is an even number");
-        } else {
-            System.out.println(num + " is an odd number");
         }
     }
-
 }
+
+
+
