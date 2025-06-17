fe6a177237fa1ff0ca1ffd926c764cbc93164c80
aisha <aishaemin33@gmail.com>
2025-06-17 12:08:39 -0400

    Created removeDuplicates

---
+++ src/Aisha/findTheUnique.java
@@ -0,0 +1,42 @@
+package Aisha;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class findTheUnique {
+
+
+    /*
+    Find the unique
+Write a return  method that can find the unique characters from the String
+Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";
+     */
+
+
+    public static String findUnique(String str){
+
+        String result="";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch=str.charAt(i);
+            int count=0;
+
+
+            for(int j=0;j<str.length();j++){
+                if(ch==str.charAt(j)){
+                    count++;
+                }
+            }
+            if(count==1){
+                result+=ch;
+            }
+        }
+
+        return result;
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("findUnique(\"AAABBBCCCDEF\") = " + findUnique("AAABBBCCCDEF"));
+    }
+}
