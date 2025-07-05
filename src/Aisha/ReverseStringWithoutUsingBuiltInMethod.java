00e472cf49292a4fd2bf7016a8845865e701f338
aisha <aishaemin33@gmail.com>
2025-07-05 16:19:19 -0400

    ReverseString&ArrayList

---
+++ src/Aisha/ReverseStringWithoutUsingBuiltInMethod.java
@@ -0,0 +1,23 @@
+package Aisha;
+
+public class ReverseStringWithoutUsingBuiltInMethod {
+
+
+
+
+    public static String REVERSED(String STR){
+
+        String reversed=" ";
+
+        for (int i = STR.length()-1; i >=0 ; i--) {
+
+            reversed+=STR.charAt(i);
+        }
+        return reversed;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("REVERSED(\"ABCD\") = " + REVERSED("ABCD"));
+    }
+}
